100153f167027d7bec0f901216cb4e674c023b00
sleaz0id <mat.staszczyk@gmail.com>
2015-12-15 17:55:43 +0100

    Solution update

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,15 @@
 def caesar_cipher(text, key)
   text.bytes.map do |x|
-  case x
-  when (65..90) then
-    x += key
-    x -= 26 if x > 90
-  when (97..122) then
-    x += key
-    x -= 26 if x > 122
-  else
-    x
-  end
+	  case x
+		  when (65..90) then
+		    x += key
+		    x -= 26 if x > 90
+		  when (97..122) then
+		    x += key
+		    x -= 26 if x > 122
+		  else
+		    x
+	  end
+	  x.chr
   end.join
 end
