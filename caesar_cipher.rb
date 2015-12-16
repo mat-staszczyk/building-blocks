6d972daf0a11b5e45824f4d2e20d7b80426420a1
sleaz0id <mat.staszczyk@gmail.com>
2015-12-16 15:17:00 +0100

    Solution update

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,20 @@
 def caesar_cipher(text, key)
   text.bytes.map do |x|
 	  case x
-		  when (65..90) then
-		    x += key
-		    x -= 26 if x > 90
-		  when (97..122) then
-		    x += key
-		    x -= 26 if x > 122
-		  else
-		    x
+		when (65..90) then
+		  x += key
+		  x -= 26 if x > 90
+	  when (97..122) then
+	    x += key
+	    x -= 26 if x > 122
+	  else
+	    x
 	  end
 	  x.chr
   end.join
 end
+
+puts "Input string:"
+string = gets.chomp
+puts "Shift factor:"
+shift = gets.chomp.to_i
