5eedfd975b9e0ed93d384ea791cfbe3db9365dc3
sleaz0id <mat.staszczyk@gmail.com>
2015-12-21 09:34:13 +0100

    Solution update

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,4 +2,5 @@ def stock_picker(array)
 	raise ArgumentError, "I need at least 2 numbers" if array.size < 2
 
 	max = array[0,2]
+	array.combination(2).map { |x| max = x if (x.last - x.first) > (max.last - max.first) }
 end
