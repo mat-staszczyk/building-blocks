0c85872f33c62eb411aca3addda759e44a3e4bc3
sleaz0id <mat.staszczyk@gmail.com>
2016-01-06 00:10:57 +0100

    Solution update

--- substrings.rb
+++ substrings.rb
@@ -2,7 +2,7 @@ def substrings(word, dictionary)
 	results = Hash.new(0) 
 	dictionary.each do |d| 
 		word.split(' ').select do |w|
-       
+      results[d] += 1 if w.downcase[d.downcase] 
     end
   end
 end
